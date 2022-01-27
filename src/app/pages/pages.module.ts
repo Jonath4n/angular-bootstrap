ac5e44da9dfc6bdbccc33ed82e7cee4f96c41d37
JonaMateBook <jgrima85@gmail.com>
2022-01-27 00:05:27 -0600

    Cards de speakers con badges

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    SpeakersComponent
   ],
   exports: [
     MainComponent
